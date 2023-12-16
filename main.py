88ad364549e99204117efb45ca1ce3ed6cc4d6e2
Niv Aiges <nivaiges@gmail.com>
2023-12-15 21:06:31 -0500

    able to get top songs from artists

--- main.py
+++ main.py
@@ -39,8 +39,26 @@ def search_for_artist(token, artist_name):
 
     query_url = url + "?" + query
     result = get(query_url, headers = headers)
-    json_result = json.loads(result.content)
-    print(json_result)
+    json_result = json.loads(result.content)["artists"]["items"]
+    if len(json_result) == 0:
+        print("No artist with this name exists")
+        return None
+    
+    return json_result[0]
+
+
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
 
 #print(token)
-search_for_artist(token, "ACDC")
+token = get_token()
+result = search_for_artist(token, "ACDC")
+artist_id = result["id"]
+songs = get_songs_by_artist(token, artist_id)
+
+for idx, song in enumerate(songs):
+    print(f"{idx + 1}. {song['name']}")
